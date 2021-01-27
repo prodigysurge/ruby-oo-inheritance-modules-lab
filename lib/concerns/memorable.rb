e3db419a523b568dcc10d04dc55521f88d993d42
prodigysurge <boatinglife6@yahoo.com>
2021-01-27 11:10:03 -0500

    commit tree 07bdeb6c81a77159e4a845c1ba37e1b7b4831eca

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,40 @@
+
+
+module Memorable
+
+    module ClassMethods
+
+        def reset_all
+            self.all.clear
+          end
+        
+          def count
+            self.all.count
+          end
+        end
+    
+    module InstanceMethods
+        def initialize
+            self.class.all << self
+        end
+    end
+end
+
+
+# module Memorable
+#     module ClassMethods
+#       def reset_all
+#         self.all.clear
+#       end
+  
+#       def count
+#         self.all.count
+#       end
+#     end
+    
+#     module InstanceMethods
+#       def initialize
+#         self.class.all << self
+#       end
+#     end
+#   end
